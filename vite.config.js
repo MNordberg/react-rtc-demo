2a62c862887bb51f2b7a3299fc7c336712cb0f6b
Matt Nordberg <mnordberg@outerlink.com>
2024-01-11 12:45:49 -0600

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/mnordberg",
+  base: "/react-rtc-demo/",
   plugins: [react()],
 })
